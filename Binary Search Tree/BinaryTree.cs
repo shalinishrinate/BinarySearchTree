ccc9037e062271cf501f0e9b8b5eab0380ba6937
Shalini Shrinate <you@example.com>
2019-12-11 15:44:01 -0600

    Working on Add method.

---
+++ Binary Search Tree/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class BinaryTree
+    {
+        //variables
+    
+        Node root;
+        Node currentNode;
+
+        //constructor
+        public BinaryTree(int data)
+        {
+            root = null;  
+        }
+
+        //method
+
+        public void Add(int data)
+        {
+            Node node = new Node();
+            if (root == null)
+            {
+                root = node;
+                return;
+            }
+            else
+            {
+                currentNode = root;
+
+                if (data < currentNode.data)
+                {
+                    if (currentNode.left == null)
+                    {
+                        currentNode.left = node;
+                    }
+                    else if (currentNode.left != null)
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                else if (data > currentNode.data)
+                {
+                    if (currentNode.right == null)
+                    {
+                        currentNode.right = node;
+                    }
+                    else if (currentNode.right != null)
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Binary Search Tree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    public class Node
+    {
+        public int data;
+
+        public Node left;
+        public Node right;
+       
+    }
+}
